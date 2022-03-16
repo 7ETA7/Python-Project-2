1289dfc82a52bdc372bf1f074cee8a8234e16f23
DeAundre Hill <dhill13@wsutech.edu>
2022-03-16 15:35:33 -0500

    python exercise7.py

---
+++ exercises/exercise7.py
@@ -0,0 +1,23 @@
+
+for i in range(1, 21):
+    print(i)
+
+
+for i in range(1, 1000001):
+    print(i)
+
+
+my_list = list(range(1, 1000001))
+print(min(my_list))
+print(max(my_list))
+print(sum(my_list))
+
+
+for i in range(1, 20, 2):
+    print(i)
+
+for i in range(3, 33, 3):
+    print(i)
+
+for i in range(1, 11):
+    print("{} cubed is {}".format(i, i ** 3))
