f3edd62534c4bd31484791901d3a7f1090f80c98
DeAundre Hill <dhill13@wsutech.edu>
2022-03-16 02:51:44 -0500

    python exercise3.py

---
+++ exercises/exercise3.py
@@ -0,0 +1,33 @@
+message = "Hey Daevon Hill do you want to learn python."
+print(message)
+
+message = "Hey daevon hill do you want to learn python."
+print(message)
+
+message = "Hey DAEVON HILL do you want to learn python."
+print(message)
+
+message = "James Brown once said, 'The one thing that can solve most of our problems is dancing.'"
+print(message)
+
+
+def famous_person():
+    print('James Brown')
+
+
+famous_person()
+
+
+def famous_person():
+    print('\nJames Brown\n')
+
+
+famous_person()
+
+
+def famous_person():
+    print('\tJames Brown\t')
+
+
+famous_person()
+
