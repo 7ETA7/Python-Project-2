4b5f2c95daf86c15ba3138cb57a91d5b6b091e21
DeAundre Hill <dhill13@wsutech.edu>
2022-03-16 07:33:44 -0500

    pythonexercise6.py

---
+++ exercises/exercise6.py
@@ -0,0 +1,73 @@
+alien_ship_color = ['green']
+
+if 'green' in alien_ship_color:
+    print("green")
+
+print("\nplayer earns 5 points!")
+
+
+alien_ship_color = ['green']
+
+if 'green' in alien_ship_color:
+    print("green")
+else:
+    print('player gets 10 points')
+
+print("\nplayer earns 5 points!")
+print("\nplayer earns 10 points!")
+
+alien_ship_color = ()
+
+green = 5
+blue = 10
+
+
+def aliens_color(green, blue):
+    if green > blue:
+        print('player earned 5 points')
+    else:
+        print('player gets 10 points')
+
+
+aliens_color
+
+green = 5
+blue = 10
+
+
+def aliens_color(arg):
+    if green == 'arg':
+        print('player earns 5 points')
+    elif blue <= 'arg':
+        print('player earns 10 points')
+
+
+aliens_color(blue)
+
+
+def stage_of_life(arg, arg2, arg3, arg4, arg5):
+    if arg >= 2:
+        print('they are a toddler')
+    elif arg2 >= 4:
+        print('they are a kid')
+    elif arg3 >= 13:
+        print('they are a teenager')
+    elif arg4 >= 20:
+        print('they are an adult')
+    elif arg5 >= 65:
+        print('they are an elder')
+    else:
+        print('not of age')
+
+
+# stage_of_life('2')
+
+
+def boolean_test(arg1, arg2):
+    if arg1 >= arg2:
+        print('true')
+
+
+boolean_test()
+
+
