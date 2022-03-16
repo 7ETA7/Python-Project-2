7427d90743cc41761a83226503afc367962ee596
DeAundre Hill <dhill13@wsutech.edu>
2022-03-15 22:20:26 -0500

    exercise2.py

---
+++ exercises/exercise2.py
@@ -0,0 +1,46 @@
+def simple():
+    print('cool')
+    print('guys')
+    print('always')
+    print('win')
+    print('first place')
+
+
+simple()
+
+
+def simple2():
+    print('bad')
+    print('gals')
+    print('never')
+    print('loose')
+
+
+simple2()
+
+
+def simple3():
+    print('okay')
+    print('people')
+    print('get')
+    print('nothing')
+
+
+simple3()
+
+
+def simple4():
+    print('crazy')
+    print('folk')
+    print('stay')
+    print('home')
+
+
+simple4()
+
+
+def message(arg1):
+    print(arg1)
+
+
+message('stop')
